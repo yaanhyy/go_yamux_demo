783af1a07e774c8a3ab5c5dc8dec8031721386e4
hyy <hyy@C02WT5SXG8WN.local>
2020-05-27 12:04:42 +0800

    add secio

---
+++ secio_server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net"
+	"context"
+	ci "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-secio"
+	"github.com/libp2p/go-libp2p-core/peer"
+		"github.com/libp2p/go-libp2p-core/sec"
+	"io"
+	)
+
+func newServerConn()(net.Conn) {
+	lstnr, err := net.Listen("tcp", "localhost:8981")
+	if err != nil {
+
+		return nil
+	}
+
+	server, err := lstnr.Accept()
+
+	lstnr.Close()
+
+	if err != nil {
+		print("Failed to accept:", err)
+	}
+
+
+
+	return server
+}
+
+func newTransport(typ, bits int) *secio.Transport {
+	priv, pub, err := ci.GenerateKeyPair(typ, bits)
+	if err != nil {
+		print(err)
+	}
+	id, err := peer.IDFromPublicKey(pub)
+	if err != nil {
+		print(err)
+	}
+	return &secio.Transport{
+		PrivateKey: priv,
+		LocalID:    id,
+	}
+}
+
+func ReadWrite( serverConn sec.SecureConn) {
+
+	after := make([]byte, 100)
+	_, err := io.ReadFull(serverConn, after)
+	if err != nil {
+		print(err)
+	}
+}
+
+
+func main()  {
+	server := newServerConn()
+	serverTpt := newTransport(ci.Ed25519, 2048)
+	serverConn, _ := serverTpt.SecureInbound(context.TODO(), server)
+	ReadWrite(serverConn)
+}
