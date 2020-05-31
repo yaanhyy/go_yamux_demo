62642b8cde699bc96983a47b0eaa3bcdf7f07bda
yaanhyy <yaanhyy@gmail.com>
2020-05-31 22:17:44 +0800

    add yamux_secio service

--- yamux_secio_client.go
+++ yamux_secio_client.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"context"
-ci "github.com/libp2p/go-libp2p-core/crypto"
-	"net"
-	"github.com/libp2p/go-libp2p-secio"
+	"github.com/hashicorp/yamux"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/sec"
-
-	"fmt"
+	"github.com/libp2p/go-libp2p-secio"
+	"net"
+"time"
+	ci "github.com/libp2p/go-libp2p-core/crypto"
 )
-
 
 func newClientConn()(net.Conn) {
 
-	client, clientErr := net.Dial("tcp", "localhost:5679")
+	client, clientErr := net.Dial("tcp", "localhost:8981")
 
 	if clientErr != nil {
 		print("Failed to accept:", clientErr)
@@ -41,19 +39,29 @@ func NewTransport(typ, bits int) *secio.Transport {
 }
 
 
-func WriteBuf( clientConn sec.SecureConn) {
-	before := []byte("hello world")
-	_, err := clientConn.Write(before)
-	if err != nil {
-		print(err)
-	}
-
-	fmt.Printf("%s", before)
-}
-
 func main()  {
+
 	client := newClientConn()
 	clientTpt := NewTransport(ci.Secp256k1, 2048)
 	clientConn, _ :=clientTpt.SecureOutbound(context.TODO(), client, "")
-	WriteBuf(clientConn)
+
+	session, _ := yamux.Client(clientConn, nil)
+
+	// 建立应用流通道1
+	stream, _ := session.Open()
+	stream.Write([]byte("ping" ))
+	stream.Write([]byte("pnng" ))
+	time.Sleep(1 * time.Second)
+
+	// 建立应用流通道2
+	stream1, _ := session.Open()
+	stream1.Write([]byte("pong"))
+	time.Sleep(1 * time.Second)
+
+	// 清理退出
+	time.Sleep(5 * time.Second)
+	stream.Close()
+	stream1.Close()
+	session.Close()
+
 }
---
+++ yamux_secio_server.go
@@ -0,0 +1,88 @@
+package main
+// 多路复用
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/yamux"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-secio"
+	"net"
+	"time"
+	ci "github.com/libp2p/go-libp2p-core/crypto"
+)
+
+func Recv(stream net.Conn, id int){
+	for {
+		buf := make([]byte, 4)
+		n, err := stream.Read(buf)
+		if err == nil{
+			fmt.Println("ID:", id, ", len:", n, time.Now().Unix(), string(buf))
+		}else{
+			fmt.Println(time.Now().Unix(), err)
+			return
+		}
+	}
+}
+
+func newServerConn()(net.Conn) {
+	lstnr, err := net.Listen("tcp", "localhost:8981")
+	if err != nil {
+
+		return nil
+	}
+
+	server, err := lstnr.Accept()
+
+	lstnr.Close()
+
+	if err != nil {
+		print("Failed to accept:", err)
+	}
+
+
+
+	return server
+}
+
+func NewTransport(typ, bits int) *secio.Transport {
+	priv, pub, err := ci.GenerateKeyPair(typ, bits)
+	if err != nil {
+		print(err)
+	}
+	id, err := peer.IDFromPublicKey(pub)
+	if err != nil {
+		print(err)
+	}
+	return &secio.Transport{
+		PrivateKey: priv,
+		LocalID:    id,
+	}
+}
+
+
+func main()  {
+	// 建立底层复用连接
+	server := newServerConn()
+	serverTpt := NewTransport(ci.Ed25519, 2048)
+	serverConn, _ := serverTpt.SecureInbound(context.TODO(), server)
+	//tcpaddr, _ := net.ResolveTCPAddr("tcp4", "127.0.0.1:8980");
+	//tcplisten, _ := net.ListenTCP("tcp", tcpaddr);
+	//conn, _ := tcplisten.Accept()
+	session, _ := yamux.Server(serverConn, nil)
+
+	id :=1
+	for {
+		// 建立多个流通路
+		stream, err := session.Accept()
+		if err == nil {
+			fmt.Println("accept")
+			id ++
+			go Recv(stream, id)
+		}else{
+			fmt.Println("session over.", err)
+			return
+		}
+	}
+
+}
+
